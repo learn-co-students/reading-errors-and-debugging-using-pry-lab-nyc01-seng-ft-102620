8908b4402289421fb672979fcc0ecc43b4041a26
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-08-21 18:17:37 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string.start_with?("s")
+    "ssssssssss" + string
   else
     string
   end
 end
+
+snake_it_up("surprise!")
