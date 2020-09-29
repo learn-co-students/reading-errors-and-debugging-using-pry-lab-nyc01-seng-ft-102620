be756daf779e73a04048e624541c41273e45e1c3
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-29 00:04:39 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
-# don't forget to add: require 'pry'
+require 'pry'
+
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string[0] = "s"
+    return ("s".to_s)*10 + string
   else
-    string
+    return string
+    
   end
 end
